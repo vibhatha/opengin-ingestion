084ef578dcd49ac6f45b04da7bebc2bbb197fb32
Vibhatha Lakmal Abeykoon <vibhatha@users.noreply.github.com>
2025-12-27 08:15:34 +0530

    fix: minor github pages title (#13)

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Automated PDF Data Extraction using GenAI">
       <HomepageHeader />
       <main>
